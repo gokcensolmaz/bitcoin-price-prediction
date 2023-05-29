f8815538d7e82b87b56150b317bffc99ba4fe4b8
Mehmet Alper KArabay <mehmetalperkarabay@TF001599.local>
2023-05-29 17:26:08 +0300

    trying merge dxy data to our model

--- kaggle/main.py
+++ kaggle/main.py
@@ -13,40 +13,33 @@ import ta
 # Neural Network library
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
+
 btc_data = pd.read_excel("btc.xlsx")
 dxy_data = pd.read_csv("dxy.csv", sep=';')
-
-
-# Convert date column in dxy_data to yyyy-mm-dd format
-dxy_data['Date'] = pd.to_datetime(dxy_data['Date'], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
-
-merged_data = pd.merge(btc_data, dxy_data, on="Date", how="inner")
-
-# Calculate percentage change of BTC and DXY prices
-btc_data['BTC_Percentage_Change'] = btc_data['Close'].pct_change()
-dxy_data['DXY_Percentage_Change'] = dxy_data['Close'].pct_change()
-
-# Merge the percentage change columns into the merged DataFrame
-merged_data = pd.merge(btc_data, dxy_data[['Date', 'DXY_Percentage_Change']], on='Date', how='left')
-
 
 ## Datetime conversion
 btc_data['Date'] = pd.to_datetime(btc_data.Date)
 
-# Setting the index
-btc_data.set_index('Date', inplace=True)
+# Convert date column in dxy_data to yyyy-mm-dd format
+dxy_data['Date'] = pd.to_datetime(dxy_data['Date'])
 
-# Dropping any NaNs
-btc_data.dropna(inplace=True)
+merged_data = pd.merge(btc_data, dxy_data, on="Date", how="inner")
 
-btc_data = ta.add_all_ta_features(btc_data, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+# Set 'Date' column as the index
+merged_data.set_index('Date', inplace=True)
+
+# Drop any NaN values
+merged_data.dropna(inplace=True)
+print(merged_data.columns)
+
+# Apply technical indicators using TA-Lib
+merged_data = ta.add_all_ta_features(merged_data, open="Open_x", high="High_x", low="Low_x", close="Close_x", volume="Volume_x", fillna=True)
+
 # Dropping everything else besides 'Close' and the Indicators
-btc_data.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
+merged_data.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
 
 # Only using the last 1000 days of data to get a more accurate representation of the current market climate
-btc_data = btc_data.tail(1000)
-
-
+merged_data = merged_data.tail(1000)
 
 ## Scaling
 
@@ -58,8 +51,7 @@ close_scaler.fit(btc_data[['Close']])
 # Normalizing/Scaling the DF
 scaler = RobustScaler()
 
-btc_data = pd.DataFrame(scaler.fit_transform(btc_data), columns=btc_data.columns, index=btc_data.index)
-
+scaled_data = pd.DataFrame(scaler.fit_transform(merged_data), columns=merged_data.columns, index=merged_data.index)
 
 def split_sequence(seq, n_steps_in, n_steps_out):
     """
@@ -87,7 +79,6 @@ def split_sequence(seq, n_steps_in, n_steps_out):
 
     return np.array(X), np.array(y)
 
-
 def visualize_training_results(results):
     """
     Plots the loss and accuracy for the training and testing data
@@ -111,8 +102,7 @@ def visualize_training_results(results):
     plt.ylabel('Accuracy')
     plt.show()
 
-
-def layer_maker(n_layers, n_nodes, activation, drop=None, d_rate=.5):
+def layer_maker(model, n_layers, n_nodes, activation, drop=None, d_rate=.5):
     """
     Creates a specified number of hidden layers for an RNN
     Optional: Adds regularization option - the dropout layer to prevent potential overfitting (if necessary)
@@ -129,8 +119,7 @@ def layer_maker(n_layers, n_nodes, activation, drop=None, d_rate=.5):
         except:
             pass
 
-
-def validater(n_per_in, n_per_out):
+def validater(model, n_per_in, n_per_out, btc_data, close_scaler):
     """
     Runs a 'For' loop to iterate through the length of the DF and create predicted values for every stated interval
     Returns a DF containing the predicted values for the model with the corresponding index values based on a business day frequency
@@ -161,7 +150,6 @@ def validater(n_per_in, n_per_out):
 
     return predictions
 
-
 def val_rmse(df1, df2):
     """
     Calculates the root mean square error between the two Dataframes
@@ -180,38 +168,31 @@ def val_rmse(df1, df2):
     # Squaring the difference and getting the mean
     rms = (df[['diff']] ** 2).mean()
 
-    # Returning the sqaure root of the root mean square
-    return np.sqrt(rms.values[0])  # <-- Here is the change
+    # Returning the square root of the root mean square
+    return np.sqrt(rms.values[0])
+
 # How many periods looking back to learn
 n_per_in  = 90
 # How many periods to predict
 n_per_out = 30
 # Features
-n_features = btc_data.shape[1]
+n_features = scaled_data.shape[1]
 # Splitting the data into appropriate sequences
-X, y = split_sequence(btc_data.to_numpy(), n_per_in, n_per_out)
+X, y = split_sequence(scaled_data.to_numpy(), n_per_in, n_per_out)
 
 ## Creating the NN
 
-# Instatiating the model
+# Instantiate the model
 model = Sequential()
 
-# Activation
-activ = "tanh"
-
 # Input layer
-model.add(LSTM(90,
-               activation=activ,
-               return_sequences=True,
-               input_shape=(n_per_in, n_features)))
+model.add(LSTM(90, activation='tanh', return_sequences=True, input_shape=(n_per_in, n_features)))
 
 # Hidden layers
-layer_maker(n_layers=1,
-            n_nodes=30,
-            activation=activ)
+layer_maker(model, n_layers=1, n_nodes=30, activation='tanh')
 
 # Final Hidden layer
-model.add(LSTM(60, activation=activ))
+model.add(LSTM(60, activation='tanh'))
 
 # Output layer
 model.add(Dense(n_per_out))
@@ -226,43 +207,37 @@ model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
 res = model.fit(X, y, epochs=50, batch_size=128, validation_split=0.1)
 
 # Transforming the actual values to their original price
-actual = pd.DataFrame(close_scaler.inverse_transform(btc_data[["Close"]]),
-                      index=btc_data.index,
-                      columns=[btc_data.columns[0]])
+actual = pd.DataFrame(close_scaler.inverse_transform(scaled_data[["Close"]]),
+                      index=scaled_data.index,
+                      columns=[scaled_data.columns[0]])
 
 # Getting a DF of the predicted values to validate against
-predictions = validater(n_per_in, n_per_out)
+predictions = validater(model, n_per_in, n_per_out, btc_data, close_scaler)
 
-# Printing the
+# Printing the RMSE
 print("RMSE:", val_rmse(actual, predictions))
 
 # Plotting
 plt.figure(figsize=(16, 6))
-
-# Plotting those predictions
 plt.plot(predictions, label='Predicted')
-
-# Plotting the actual values
 plt.plot(actual, label='Actual')
-
 plt.title(f"Predicted vs Actual Closing Prices")
 plt.ylabel("Price")
 plt.legend()
-plt.xlim(datetime.datetime.strptime('2018-05', '%Y-%m'), datetime.datetime.strptime('2020-05', '%Y-%m'))
 plt.show()
 
 # Predicting off of the most recent days from the original DF
-yhat = model.predict(np.array(btc_data.tail(n_per_in)).reshape(1, n_per_in, n_features))
+yhat = model.predict(np.array(merged_data.tail(n_per_in)).reshape(1, n_per_in, n_features))
 
 # Transforming the predicted values back to their original format
 yhat = close_scaler.inverse_transform(yhat)[0]
 
 # Creating a DF of the predicted prices
 preds = pd.DataFrame(yhat,
-                     index=pd.date_range(start=btc_data.index[-1] + timedelta(days=1),
+                     index=pd.date_range(start=merged_data.index[-1] + timedelta(days=1),
                                          periods=len(yhat),
                                          freq="B"),
-                     columns=[btc_data.columns[0]])
+                     columns=[merged_data.columns[0]])
 
 # Number of periods back to plot the actual values
 pers = n_per_in
@@ -271,12 +246,12 @@ pers = n_per_in
 actual = pd.DataFrame(close_scaler.inverse_transform(btc_data[["Close"]].tail(pers)),
                       index=btc_data.Close.tail(pers).index,
                       columns=[btc_data.columns[0]])
-actual = pd.concat([actual, preds.head(1)])
+
 # Printing the predicted prices
 print(preds)
 
 # Plotting
-plt.figure(figsize=(16,6))
+plt.figure(figsize=(16, 6))
 plt.plot(actual, label="Actual Prices")
 plt.plot(preds, label="Predicted Prices")
 plt.ylabel("Price")
@@ -284,4 +259,3 @@ plt.xlabel("Dates")
 plt.title(f"Forecasting the next {len(yhat)} days")
 plt.legend()
 plt.show()
-
